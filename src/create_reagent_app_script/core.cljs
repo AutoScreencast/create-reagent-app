84d7acb519a50f7d8c7654e08819713bd5f82d1c
Tom Bowden <tombowden@Toms-MacBook-Pro.local>
2020-11-17 22:27:59 +0900

    Update v0.0.20

--- src/create_reagent_app_script/core.cljs
+++ src/create_reagent_app_script/core.cljs
@@ -3,13 +3,6 @@
             [cljs-node-io.core :refer [slurp]]
             [fs :refer [existsSync mkdirSync appendFileSync]]))
 
-;; Constant filepaths
-(def FILEPATH_BASIC_TEMPLATE_SHADOW_CLJS_EDN          "./templates/basic/shadow-cljs.edn")
-(def FILEPATH_BASIC_TEMPLATE_PACKAGE_JSON             "./templates/basic/package.json")
-(def FILEPATH_BASIC_TEMPLATE_DOT_GITIGNORE            "./templates/basic/.gitignore")
-(def FILEPATH_BASIC_TEMPLATE_PUBLIC_INDEX_HTML        "./templates/basic/public/index.html")
-(def FILEPATH_BASIC_TEMPLATE_PUBLIC_CSS_STYLE_CSS     "./templates/basic/public/css/style.css")
-(def FILEPATH_BASIC_TEMPLATE_SRC_MY_APP_APP_CORE_CLJS "./templates/basic/src/my_app/app/core.cljs")
 
 ;; Get imput command line arguments from Node process
 (def arguments (js->clj js/process.argv))
@@ -17,7 +10,28 @@
 ;; `npx create-react-app my-app --re-frame` will give us options of [my-app --re-frame]
 (def options (subvec arguments 2))
 
+;; eg. "my-app"
 (def user-project-name (str (first options)))
+
+(println (str "user-project-name:" user-project-name))
+
+;; Current working directory - assume the process is launched from the root level of project...
+(def CWD (. js/process cwd))
+
+(println (str "cwd:" CWD))
+
+(println "create-reagent-app version: 0.0.24")
+
+;; Constant filepaths
+
+(def FILEPATH_BASIC_TEMPLATE_SHADOW_CLJS_EDN          (str CWD "/templates/basic/shadow-cljs.edn"))
+(def FILEPATH_BASIC_TEMPLATE_PACKAGE_JSON             (str CWD "/templates/basic/package.json"))
+(def FILEPATH_BASIC_TEMPLATE_DOT_GITIGNORE            (str CWD "/templates/basic/.gitignore"))
+(def FILEPATH_BASIC_TEMPLATE_PUBLIC_INDEX_HTML        (str CWD "/templates/basic/public/index.html"))
+(def FILEPATH_BASIC_TEMPLATE_PUBLIC_CSS_STYLE_CSS     (str CWD "/templates/basic/public/css/style.css"))
+(def FILEPATH_BASIC_TEMPLATE_SRC_MY_APP_APP_CORE_CLJS (str CWD "/templates/basic/src/my_app/app/core.cljs"))
+
+;; Utility Functions
 
 (defn maybe-create-folder! [folder-name]
   (if (not (existsSync folder-name))
@@ -32,73 +46,69 @@
 
 ;; --- Linear sequence of steps ---
 
-;; Create `my-app` folder.
-(maybe-create-folder! user-project-name)
+;; Output folders
+(def FILEPATH_USER_PROJECT_NAME_FOLDER (str CWD "/" user-project-name))
+(def FILEPATH_PUBLIC_FOLDER            (str FILEPATH_USER_PROJECT_NAME_FOLDER "/public"))
+(def FILEPATH_PUBLIC_CSS_FOLDER        (str FILEPATH_PUBLIC_FOLDER "/css"))
+(def FILEPATH_SRC_FOLDER               (str FILEPATH_USER_PROJECT_NAME_FOLDER "/src"))
+(def FILEPATH_SRC_UPN_FOLDER           (str FILEPATH_SRC_FOLDER "/" (replace-hyphens-with-underscores user-project-name)))
+(def FILEPATH_SRC_UPN_APP_FOLDER       (str FILEPATH_SRC_UPN_FOLDER "/app"))
 
-;; Create `shadow-cljs.edn` file and its contents
+;; Create folders
+(maybe-create-folder! FILEPATH_USER_PROJECT_NAME_FOLDER)
+(maybe-create-folder! FILEPATH_PUBLIC_FOLDER)
+(maybe-create-folder! FILEPATH_PUBLIC_CSS_FOLDER)
+(maybe-create-folder! FILEPATH_SRC_FOLDER)
+(maybe-create-folder! FILEPATH_SRC_UPN_FOLDER)
+(maybe-create-folder! FILEPATH_SRC_UPN_APP_FOLDER)
+
+;; Output files
+(def FILEPATH_USER_PROJECT_NAME_FOLDER_SHADOW_CLJS_EDN (str FILEPATH_USER_PROJECT_NAME_FOLDER "/shadow-cljs.edn"))
+(def FILEPATH_USER_PROJECT_NAME_FOLDER_PACKAGE_JSON    (str FILEPATH_USER_PROJECT_NAME_FOLDER "/package.json"))
+(def FILEPATH_USER_PROJECT_NAME_FOLDER_DOT_GITIGNORE   (str FILEPATH_USER_PROJECT_NAME_FOLDER "/.gitignore"))
+(def FILEPATH_PUBLIC_FOLDER_INDEX_HTML                 (str FILEPATH_PUBLIC_FOLDER "/index.html"))
+(def FILEPATH_PUBLIC_CSS_FOLDER_STYLE_CSS              (str FILEPATH_PUBLIC_CSS_FOLDER "/style.css"))
+(def FILEPATH_SRC_UPN_APP_FOLDER_CORE_CLJS             (str FILEPATH_SRC_UPN_APP_FOLDER "/core.cljs"))
+
+;; Create files with content
 (append-contents-to-file!
- (str "./" user-project-name "/shadow-cljs.edn")
+ FILEPATH_USER_PROJECT_NAME_FOLDER_SHADOW_CLJS_EDN
  (str/replace (slurp FILEPATH_BASIC_TEMPLATE_SHADOW_CLJS_EDN) "*|USER-PROJECT-NAME|*" user-project-name))
 
-;; Create `package.json` file and its contents
 (append-contents-to-file!
- (str "./" user-project-name "/package.json")
+ FILEPATH_USER_PROJECT_NAME_FOLDER_PACKAGE_JSON
  (str/replace (slurp FILEPATH_BASIC_TEMPLATE_PACKAGE_JSON) "*|USER-PROJECT-NAME|*" user-project-name))
 
-;; Create `.gitignore` file and its contents
 (append-contents-to-file!
- (str "./" user-project-name "/.gitignore")
+ FILEPATH_USER_PROJECT_NAME_FOLDER_DOT_GITIGNORE
  (slurp FILEPATH_BASIC_TEMPLATE_DOT_GITIGNORE))
 
-;; Create `public` folder
-(maybe-create-folder! (str user-project-name "/public"))
-
-;; Create `index.html` file
 (append-contents-to-file!
- (str "./" user-project-name "/public/index.html")
+ FILEPATH_PUBLIC_FOLDER_INDEX_HTML
  (slurp FILEPATH_BASIC_TEMPLATE_PUBLIC_INDEX_HTML))
 
-;; Create `css` folder under `public`
-(maybe-create-folder! (str user-project-name "/public/css"))
-
-;; Create `style.css` file
 (append-contents-to-file!
- (str "./" user-project-name "/public/css/style.css")
+ FILEPATH_PUBLIC_CSS_FOLDER_STYLE_CSS
  (slurp FILEPATH_BASIC_TEMPLATE_PUBLIC_CSS_STYLE_CSS))
-
-;; Create `src` folder
-(maybe-create-folder! (str user-project-name "/src"))
-
-;; Create `user-project-name` folder, with underscores instead of hyphens, if any
-(maybe-create-folder! (str user-project-name
-                           "/src/"
-                           (replace-hyphens-with-underscores user-project-name)))
-
-;; Create `app` folder
-(maybe-create-folder! (str user-project-name
-                           "/src/"
-                           (replace-hyphens-with-underscores user-project-name)
-                           "/app"))
 
 ;; Create `core.cljs` file
 (append-contents-to-file!
- (str "./" user-project-name
-      "/src/"
-      (replace-hyphens-with-underscores user-project-name)
-      "/app/core.cljs")
+ FILEPATH_SRC_UPN_APP_FOLDER_CORE_CLJS
  (str/replace (slurp FILEPATH_BASIC_TEMPLATE_SRC_MY_APP_APP_CORE_CLJS) "*|USER-PROJECT-NAME|*" user-project-name))
 
 ;; --- Run ---
 
 ;; FIXME: Not making any allowances for anything going wrong...
 (defn output! []
-  (println "====================================== CREATE REAGENT APP ======================================")
+  (println "")
+  (println "================================== CREATE REAGENT APP v0.0.23 ==================================")
   (println (str "Your app `" user-project-name "` was successfully created. Please do the following 4 steps:"))
   (println (str "1. Please change into the project folder: `cd " user-project-name "`"))
   (println "2. Then, install the package dependencies using npm or yarn: `npm install` or `yarn install`")
   (println "3. Run your app with `npm start` or `yarn start`")
   (println "4. Open your app in the browser at `localhost:3000`")
-  (println "================================================================================================"))
+  (println "================================================================================================")
+  (println ""))
 
 (defn ^:export main []
   (output!))
